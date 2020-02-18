66367731dfe96a382d4467bc21b41f9d35b52ec1
juanagustin0504 <juanagustin@naver.com>
2020-02-18 17:23:11 +0900

    - add UITextField shaking

--- StudyToAPI/Application/Main/MainViewController.swift
+++ StudyToAPI/Application/Main/MainViewController.swift
@@ -43,30 +43,39 @@ class MainViewController: UIViewController {
         guard let userId = idText.text, let userPw = pwText.text else {
             return
         }
-        
+
         loginViewModel.requestLogin(usrID: userId, usrPW: userPw) { (error) in
             if error != nil {
                 self.showAlert(message: "error: \(String(describing: error))")
                 return
             }
-            
+
             switch self.loginViewModel.loginModel?.RESULT_TP {
             case "0":
                 DispatchQueue.main.async {
                     self.showAlert(message: "로그인 성공")
-                    
+
                     self.loginBtn.setTitle(self.loginViewModel.loginModel?.USR_NM, for: .normal)
                     self.idText.isEnabled = false
                     self.pwText.isEnabled = false
                     self.loginBtn.isEnabled = false
                 }
             case "1":
-                self.showAlert(message: "실패(아이디 없음)")
+                DispatchQueue.main.async {
+                    self.idText.shake {
+                        self.idText.layer.borderColor = UIColor.black.cgColor
+                    }
+                }
             case "2":
-                self.showAlert(message: "실패(비밀번호 불일치)")
+                DispatchQueue.main.async {
+                    self.pwText.shake() {
+                        
+                    }
+                }
             default:
                 self.showAlert(message: "그 밖의 오류")
             }
+
         }
         
     }
@@ -100,7 +109,7 @@ class MainViewController: UIViewController {
          
     }
     
-    //MARK: - Alert -
+    //MARK: - custom method -
     func showAlert(title: String = "알림", message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
@@ -111,6 +120,27 @@ class MainViewController: UIViewController {
         }
         
     }
-        
 
+}
+
+//MARK: - extension -
+extension UITextField {
+    func shake(completion: @escaping () -> Void) {
+        
+        self.border(color: UIColor.red)
+        
+        let animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.05
+        animation.repeatCount = 5
+        animation.autoreverses = true
+        animation.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
+        animation.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        layer.add(animation, forKey: "position")
+    }
+    
+    func border(color: UIColor) {
+        self.layer.borderColor = color.cgColor
+        self.layer.borderWidth = 1.0
+        self.layer.cornerRadius = 5
+    }
 }
